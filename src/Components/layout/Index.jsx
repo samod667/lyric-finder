52698a171119910887e19af8414f1ca7a0050717
dorsamoha <dorsamoha@gmail.com>
2020-07-06 13:50:37 +0300

    first commit

---
+++ src/Components/layout/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tracks from '../tracks/Tracks';
+import Search from '../tracks/Search';
+
+export default function Index() {
+    return (
+       <React.Fragment>
+       <Search />
+       <Tracks />
+       </React.Fragment>
+    )
+}
